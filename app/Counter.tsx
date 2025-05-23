0337d830469b08388bda2a59ac30cde9386cb15a
Chris Krogh <christopher.krogh@faire.com>
2025-05-23 16:31:18 -0400

    lodash makes it in

--- app/Counter.tsx
+++ app/Counter.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useState } from "react";
-import { Count } from "./types";
+import { Count, INITIAL_COUNT } from "./types";
 
 export default function Counter() {
-  const [count, setCount] = useState<Count>({ count: 0 });
+  const [count, setCount] = useState<Count>({ count: INITIAL_COUNT });
   return (
     <div>
       <h1>Count: {count.count}</h1>
--- app/types.ts
+++ app/types.ts
@@ -5,3 +5,9 @@ export const SOMETHING = _.noop;
 export type Count = {
   count: number;
 };
+
+/**
+ * If this is imported by Counter, lodash will be bundled into the client bundle.
+ * (even though it's not used in the client)
+ */
+export const INITIAL_COUNT = 0;
