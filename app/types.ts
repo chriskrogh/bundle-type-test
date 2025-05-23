329661dca1ad8d09341fc01520803ee465e8997c
Chris Krogh <christopher.krogh@faire.com>
2025-05-23 16:46:17 -0400

    Update README.md

--- app/types.ts
+++ app/types.ts
@@ -3,12 +3,13 @@ import _ from "lodash";
 // Unused export
 export const SOMETHING = _.noop;
 
+// Importing this doesn't bring in lodash
 export type Count = {
   count: number;
 };
 
 /**
- * If this is imported by Counter, lodash will be bundled into the client bundle.
+ * If this is imported, lodash will be bundled into the client bundle.
  * (even though it's not used in the client)
  */
 export const INITIAL_COUNT = 0;
